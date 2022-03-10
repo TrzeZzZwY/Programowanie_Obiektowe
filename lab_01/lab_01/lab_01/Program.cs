78dc1352e85cff20e7e693cf12a5a83828a6a8d5
jan.swider <jan.swider@PK5-09>
2022-03-10 13:18:24 +0100

    koniec lekcji

--- lab_01/lab_01/lab_01/Program.cs
+++ lab_01/lab_01/lab_01/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace lab_01
 {
     class Program
     {
+        //https://dirask.com/posts/WSEI-2021-2022-lato-labN-1-ISN-Programowanie-obiektowe-lab-1-pa3ek1
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Ułamek a = new Ułamek(1, 2);
+            Ułamek b = new Ułamek(3, 2);
+
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+
+            Ułamek c = new Ułamek(5, 2);
+            Ułamek d = new Ułamek(6, 2);
+            Ułamek e = new Ułamek(6, 2);
+            Ułamek f = new Ułamek(2, 2);
+            Ułamek g = new Ułamek(-1, 2);
+
+
+            Console.WriteLine(a.CompareTo(g));
+
+
+
+            List<Ułamek> lista = new List<Ułamek>() {a,b,c,d,e,f,g};
+            Console.WriteLine("przed sortem");
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            lista.Sort();
+            Console.WriteLine("\npo sorcie");
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nZaokrągl w dół");
+            Console.WriteLine(c.RoundDown());
+            Console.WriteLine("\nZaokrągl w górę");
+            Console.WriteLine(c.RoundUp());
         }
     }
 }
---
+++ lab_01/lab_01/lab_01/Ułamek.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_01
+{
+    public class Ułamek : IEquatable<Ułamek>, IComparable<Ułamek>
+    {
+        private int licznik;
+        private int mianownik;
+        public int Licznik { get => licznik; }
+        public int Mianownik { get => Mianownik; }
+        public Ułamek()
+        {
+            licznik = default;
+            mianownik = default;
+        }
+        public Ułamek(int licznik,int mianownik)
+        {
+            this.licznik = licznik;
+            this.mianownik = mianownik;
+        }
+
+        public Ułamek(Ułamek obj)
+        {
+            licznik = obj.licznik;
+            mianownik = obj.mianownik;
+        }
+
+        public int RoundUp() => (int)Math.Ceiling((double)this.licznik / this.mianownik);
+        public int RoundDown() => (int)Math.Floor((double)this.licznik / this.mianownik);
+
+        public override string ToString() => $"licznik: {licznik}, ,mianownik {mianownik}";
+
+        public bool Equals(Ułamek other) => (double)this.licznik / this.mianownik == (double)other.licznik / other.mianownik;
+
+
+        public int CompareTo(Ułamek other)
+        {
+            if (this.Equals(other))
+                return 0;
+            if ((double)this.licznik / this.mianownik < (double)other.licznik / other.mianownik)
+                return -1;
+            else
+                return 1;
+
+        }
+
+        public static Ułamek operator +(Ułamek a, Ułamek b)
+        {
+            if(a.mianownik == b.mianownik)
+                return new Ułamek(a.licznik + b.licznik, a.mianownik);
+            else
+                return new Ułamek((a.licznik * b.mianownik) + (b.licznik * a.mianownik) , a.mianownik * b.mianownik);
+        }
+        public static Ułamek operator -(Ułamek a, Ułamek b)
+        {
+            if (a.mianownik == b.mianownik)
+                return new Ułamek(a.licznik - b.licznik, a.mianownik);
+            else
+                return new Ułamek((a.licznik * b.mianownik) - (b.licznik * a.mianownik), a.mianownik * b.mianownik);
+        }
+        public static Ułamek operator *(Ułamek a, Ułamek b) => (new Ułamek(a.licznik * b.licznik, a.mianownik * b.mianownik));
+        public static Ułamek operator /(Ułamek a, Ułamek b) => (new Ułamek(a.licznik * b.mianownik, a.mianownik * b.licznik));
+    }
+}
